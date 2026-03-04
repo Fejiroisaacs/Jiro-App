c564009584254757575c6c0f0baecdc9acfd696b
Oghenefejiro Anigboro <Fejiroisaac@gmail.com>
2026-03-03 22:19:17 -0500

    added ledger db migrations

---
+++ jiro-api/migrations/000028_create_ledger.down.sql
@@ -0,0 +1,5 @@
+DROP TABLE IF EXISTS ledger_networth_snapshots CASCADE;
+DROP TABLE IF EXISTS ledger_budgets CASCADE;
+DROP TABLE IF EXISTS ledger_transactions CASCADE;
+DROP TABLE IF EXISTS ledger_categories CASCADE;
+DROP TABLE IF EXISTS ledger_accounts CASCADE;
